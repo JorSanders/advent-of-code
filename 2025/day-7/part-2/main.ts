d7466ef3d862120654f7d02616a91134d952b61d
Jor Sanders <it@jor.dev>
2025-12-08 00:57:15 +0100

    feat: add 2025 day 7 part 2 not working

---
+++ 2025/day-7/part-2/main.ts
@@ -0,0 +1,64 @@
+import fs from "node:fs";
+
+const lines = fs
+  .readFileSync("2025/day-7/part-2/input.txt", "utf-8")
+  .trim()
+  .split("\n");
+
+let result = 0;
+
+let startIndex = 0;
+
+const startLine = lines[0];
+
+[...startLine].forEach((char, charIndex) => {
+  if (char === "S") {
+    startIndex = charIndex;
+  }
+});
+
+let beamIndexes = new Map<number, number>();
+
+beamIndexes.set(startIndex, 1);
+
+for (let i = 1; i < lines.length; i++) {
+  let newIndexes = new Map<number, number>();
+  const chars = [...lines[i]];
+
+  beamIndexes.forEach((value, key) => {
+    const char = chars[key];
+
+    if (char === ".") {
+      newIndexes.set(key, value);
+    } else {
+      const left = newIndexes.get(key - 1);
+      const leftValue = (left ?? 0) + 1;
+      newIndexes.set(key - 1, leftValue);
+
+      const right = newIndexes.get(key + 1);
+      const rightValue = (right ?? 0) + 1;
+      newIndexes.set(key + 1, rightValue);
+    }
+  });
+
+  const debugChars: string[] = [];
+
+  for (let j = 0; j < lines[i].length; j++) {
+    const beam = newIndexes.get(j);
+    if (beam) {
+      debugChars.push(beam.toString());
+    } else {
+      debugChars.push(chars[j]);
+    }
+  }
+
+  console.log(debugChars.join(""));
+
+  beamIndexes = newIndexes;
+}
+
+beamIndexes.forEach((value, _key) => {
+  result += value;
+});
+
+console.log("result", result);
